e8d6f004c597036c01bd6a8f2707ed12dd192b7c
Marina Moshnogorskaya <marinamoshnogorskaya@gmail.com>
2018-06-06 11:31:17 +0300

    got input values

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -9,19 +9,34 @@ import { RepoService } from '../repo.service';
 })
 export class SearchComponent implements OnInit {
   repos: Repo[];
+  type: string;
+  language: string;
+  searchTerm: string;
 
   constructor(private repoService: RepoService) { }
+
+  updateType(value: string): void{
+    this.type = value;
+    console.log(this.type);
+  }
+
+  updateLanguage(value: string): void{
+    this.language = value;
+    console.log(this.language);
+  }
+
+  updateSearchTerm(value: string): void{
+    this.searchTerm = value;
+    console.log(this.searchTerm);
+  }
+
+  getURL(): string {
+    return 'hi';
+  }
+
     getRepos(): void {
       this.repoService.getRepos()
         .subscribe(repos => this.repos = repos);
-    }
-
-    update(value: string){
-      console.log(value);
-      return value;
-    }
-    getURL(): string {
-      return 'hi';
     }
 
   ngOnInit() {
