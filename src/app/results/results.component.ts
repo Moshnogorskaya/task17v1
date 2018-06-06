3340549bd1bbcc52bb92650989262aa6dedcebfa
Marina Moshnogorskaya <marinamoshnogorskaya@gmail.com>
2018-06-06 13:29:55 +0300

    splitted components work

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Repo } from '../repo';
+import { RepoService } from '../repo.service';
 
 @Component({
   selector: 'app-results',
+  inputs: ['repos'],
   templateUrl: './results.component.html',
   styleUrls: ['./results.component.css']
 })
 export class ResultsComponent implements OnInit {
-
   constructor() { }
-
   ngOnInit() {
   }
 
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { Repo } from '../repo';
-import { RepoService } from '../repo.service';
+import { Component, OnInit } from "@angular/core";
+
+import { Repo } from "../repo";
+import { RepoService } from "../repo.service";
+import { ResultsComponent } from "../results/results.component";
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  selector: "app-search",
+  templateUrl: "./search.component.html",
+  styleUrls: ["./search.component.css"]
 })
 export class SearchComponent implements OnInit {
   repos: Repo[];
@@ -14,30 +16,32 @@ export class SearchComponent implements OnInit {
   searchTerm: string;
   url: string;
 
-  constructor(private repoService: RepoService) { }
+  constructor(private repoService: RepoService) {}
 
-  updateType(value: string): void{
+  updateType(value: string): void {
     this.type = value.toLowerCase();
   }
 
-  updateLanguage(value: string): void{
+  updateLanguage(value: string): void {
     this.language = value.toLowerCase();
   }
 
-  updateSearchTerm(value: string): void{
+  updateSearchTerm(value: string): void {
     this.searchTerm = value.toLowerCase();
   }
 
-    getRepos(): boolean {
-      this.url = `https://api.github.com/search/${this.type}?q=${this.searchTerm}+language:${this.language}&sort=stars&order=desc`;
-      this.repoService.getRepos(this.url)
-        .subscribe(repos => this.repos = repos);
-        console.log('get repos done');
-        return false;
-    }
+  getRepos(): boolean {
+    this.url = `https://api.github.com/search/${this.type}?q=${
+      this.searchTerm
+    }+language:${this.language}&sort=stars&order=desc`;
+    this.repoService
+      .getRepos(this.url)
+      .subscribe(repos => (this.repos = repos));
+    console.log("get repos done");
+    return false;
+  }
 
   ngOnInit() {
     this.getRepos();
   }
-
 }
