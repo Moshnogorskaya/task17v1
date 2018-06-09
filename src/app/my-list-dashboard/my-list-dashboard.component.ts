641a427db8bf4b468a09a52b3946867cc035c787
Marina Moshnogorskaya <marinamoshnogorskaya@gmail.com>
2018-06-09 11:12:16 +0300

    works like before with added components

--- src/app/my-list-dashboard/my-list-dashboard.component.ts
+++ src/app/my-list-dashboard/my-list-dashboard.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Repo } from "../repo";
+import { RepoService } from "../repo.service";
 
 @Component({
   selector: 'app-my-list-dashboard',
+  inputs: ['repos'],
   templateUrl: './my-list-dashboard.component.html',
   styleUrls: ['./my-list-dashboard.component.css']
 })
 export class MyListDashboardComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(public repoService: RepoService) { }
+  
+  addRepo(repo){
+    this.repoService.addRepo(repo)   
+        repo.archived = true;
   }
+  
+  deleteRepo(repo) {
+    this.repoService.deleteRepo(repo)
+        repo.archived = false;
+  }
+  
+    ngOnInit() {
+    }
 
 }
--- src/app/my-list-list/my-list-list.component.ts
+++ src/app/my-list-list/my-list-list.component.ts
@@ -1,13 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Repo } from "../repo";
+import { RepoService } from "../repo.service";
 
 @Component({
   selector: 'app-my-list-list',
+  inputs: ['repos'],
   templateUrl: './my-list-list.component.html',
   styleUrls: ['./my-list-list.component.css']
 })
 export class MyListListComponent implements OnInit {
 
-  constructor() { }
+  constructor(public repoService: RepoService) { }
+  onClick(repo: Repo) {
+    if(!repo.archived) {
+      this.repoService.addRepo(repo)   
+      repo.archived = true;
+    }else{
+      this.repoService.deleteRepo(repo)
+      repo.archived = false;
+    }
+  }
 
   ngOnInit() {
   }
