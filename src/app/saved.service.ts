0f46ddeff26ebaba9ae8d6a752890d14db1b09c0
Marina Moshnogorskaya <marinamoshnogorskaya@gmail.com>
2018-06-13 15:45:27 +0300

    filled new service

--- src/app/saved.service.ts
+++ src/app/saved.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Repo } from "./repo";
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,23 @@ import { Injectable } from '@angular/core';
 export class SavedService {
 
   constructor() { }
+
+  private savedRepos: Repo[] = [];
+
+  addRepo(repo: Repo): Repo[] {
+    this.deleteRepo(repo).push(repo);
+    console.log('repos after adding!', this.savedRepos);
+    return this.savedRepos;
+  }
+  
+  deleteRepo(repo: Repo): Repo[] {
+    let oldRepos = [...this.savedRepos];
+    this.savedRepos = oldRepos.filter(item => item.id !== repo.id);
+    console.log('repos after deleting!', this.savedRepos);
+    return this.savedRepos;
+  }
+
+  getSavedRepos(): Repo[] {
+    return [...this.savedRepos];
+  }
 }
