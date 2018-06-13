1108491b822933ecfe274e9fd9ee60947d3b0bba
Marina Moshnogorskaya <marinamoshnogorskaya@gmail.com>
2018-06-13 13:46:07 +0300

    directive is updated

--- src/app/update-repos.directive.ts
+++ src/app/update-repos.directive.ts
@@ -1,10 +1,17 @@
 import { Directive } from '@angular/core';
+import { RepoService } from "../app/repo.service";
+import { Repo } from "../app/repo";
 
 @Directive({
   selector: '[appUpdateRepos]'
 })
 export class UpdateReposDirective {
 
-  constructor() { }
+  constructor(public repoService: RepoService) { }
 
+
+  deleteRepo(repo: Repo): void {
+    this.repoService.deleteRepo(repo);
+    repo.archived = false;
+  }
 }
